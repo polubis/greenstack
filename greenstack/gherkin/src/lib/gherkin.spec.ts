fdb5da5a4d88b730ebfe5ec4e2884a6316ad4889
polubis <polubis@wp.pl>
2024-04-25 07:19:44 +0200

    feat(gherkin): finish first version of library

--- greenstack/gherkin/src/lib/gherkin.spec.ts
+++
@@ -1,7 +0,0 @@
-import { gherkin } from './gherkin';
-
-describe('gherkin', () => {
-  it('should work', () => {
-    expect(gherkin()).toEqual('gherkin');
-  });
-});
--- greenstack/gherkin/src/lib/gherkin.ts
+++ greenstack/gherkin/src/lib/gherkin.ts
@@ -1,3 +1,80 @@
-export function gherkin(): string {
-  return 'gherkin';
+type GherkinFnName = 'Given' | 'When' | 'Then' | 'And';
+type MiddlewareFunction<Key extends string | symbol | number> = (
+  name: GherkinFnName,
+  key: Key
+) => void;
+
+type Commands<C extends Record<string, unknown>> = Record<
+  keyof C,
+  (...args: never[]) => void
+>;
+
+interface Setup<C extends Record<string, unknown>> {
+  commands: Commands<C>;
+  middleware?: MiddlewareFunction<keyof C>[];
 }
+
+function Gherkin<S extends Setup<S['commands']>>({ commands, middleware }: S) {
+  function runMiddleware(name: GherkinFnName, key: keyof S['commands']): void {
+    middleware?.forEach((fn) => fn(name, key));
+  }
+
+  function Given<K extends keyof S['commands']>(
+    key: K,
+    ...args: Parameters<S['commands'][K]>
+  ) {
+    runMiddleware('Given', key);
+    commands[key](...args);
+
+    return {
+      Then,
+      When,
+      And,
+    };
+  }
+
+  function Then<K extends keyof S['commands']>(
+    key: K,
+    ...args: Parameters<S['commands'][K]>
+  ) {
+    runMiddleware('Then', key);
+    commands[key](...args);
+
+    return {
+      And,
+      When,
+    };
+  }
+
+  function When<K extends keyof S['commands']>(
+    key: K,
+    ...args: Parameters<S['commands'][K]>
+  ) {
+    runMiddleware('When', key);
+    commands[key](...args);
+
+    return {
+      And,
+      Then,
+    };
+  }
+
+  function And<K extends keyof S['commands']>(
+    key: K,
+    ...args: Parameters<S['commands'][K]>
+  ) {
+    runMiddleware('And', key);
+    commands[key](...args);
+
+    return {
+      Then,
+      When,
+      And,
+    };
+  }
+
+  return { Given };
+}
+
+export type { Setup, Commands, MiddlewareFunction };
+export { Gherkin };
