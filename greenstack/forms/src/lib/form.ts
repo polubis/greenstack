3bf2d6d35614f790a40eb34dea2bbaa2d4d54f8e
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-05-06 08:11:26 +0200

    wip

--- greenstack/forms/src/lib/form.ts
+++ greenstack/forms/src/lib/form.ts
@@ -1,77 +1,71 @@
-type ValidationResult<Identifier extends string = string> = Identifier | ' ';
-// Returns truthy if value is valid.
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-type ValidatorsRegistryFn = (
-  ...args: any[]
-) => (value: unknown) => ValidationResult;
-
-type ValidatorsRegistryBase = Record<string, ValidatorsRegistryFn>;
+/* eslint-disable @typescript-eslint/no-explicit-any */
+type ValidationResult<Identifier extends string> = Identifier | null;
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 type ValuesBase = Record<string | number, any>;
 
 type ValidatorsSetup<Values extends ValuesBase> = {
-  [Key in keyof ValuesBase]?: (value: Values[Key]) => ValidationResult;
+  [Key in keyof Values]?: ((value: Values[Key]) => ValidationResult<string>)[];
 };
 
-const form =
-  <ValidatorsRegistry extends ValidatorsRegistryBase>(
-    registry: ValidatorsRegistry
-  ) =>
-  <Values extends ValuesBase>(
-    validatorsSetup: ValidatorsSetup<Values> = {}
-  ) => {
-    let values: Values;
+type ValuesKeys<Values extends ValuesBase> = (keyof Values)[];
 
-    return {
-      init: (initialValues: Values) => {
-        values = initialValues;
-      },
-      set: <Key extends keyof Values>(key: Key, value: Values[Key]) => {
-        values = {
-          ...values,
-          [key]: value,
-        };
-      },
-      patch: (newValues: Partial<Values>) => {
-        values = {
-          ...values,
-          ...newValues,
-        };
-      },
-    };
+type ValidationReport<Values extends ValuesBase> = {
+  [Key in keyof Values]: ValidationResult<string>;
+};
+
+const form = <Values extends ValuesBase>(
+  validatorsSetup: ValidatorsSetup<Values> = {}
+) => {
+  let values: Values;
+  let keys: ValuesKeys<Values>;
+  let report: ValidationReport<Values>;
+
+  return {
+    init: (initialValues: Values) => {
+      values = initialValues;
+      keys = Object.keys(values);
+    },
+    set: <Key extends keyof Values>(key: Key, value: Values[Key]) => {
+      values = {
+        ...values,
+        [key]: value,
+      };
+    },
+    patch: (newValues: Partial<Values>) => {
+      values = {
+        ...values,
+        ...newValues,
+      };
+    },
+    report,
   };
+};
 
 const min =
   (limit: number) =>
-  (value: unknown): ValidationResult<'min'> => {
-    if (typeof value !== `string` && !Array.isArray(value)) return ` `;
-
-    return value.length > limit ? `min` : ` `;
-  };
+  (value: string | unknown[]): ValidationResult<'min'> =>
+    value.length > limit ? `min` : null;
 
 const max =
   (limit: number) =>
-  (value: unknown): ValidationResult<'max'> => {
-    if (typeof value !== `string` && !Array.isArray(value)) return ` `;
-
-    return value.length > limit ? `max` : ` `;
-  };
-
-const createForm = form({
-  min,
-  max,
-});
+  (value: string | unknown[]): ValidationResult<'max'> =>
+    value.length > limit ? `max` : null;
 
 interface RegisterFormData {
   username: string;
   password: string;
 }
 
-const registerForm = createForm<RegisterFormData>({});
+const registerForm = form<RegisterFormData>({
+  username: [],
+  password: [min(15), max(20)],
+});
 // Performs validation initially.
 registerForm.init({ username: ``, password: `` });
 
+// registerForm.report.password.
+// registerForm.report.password?.charAt();
 // const [form] = useState(registerForm.init)
 // const [state, setState] = useState(form.state)
 
